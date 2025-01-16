9e98facf75720c170c112e62d294340c82d7e3aa
sri0461999 <sri0461999@hmail.com>
2025-01-16 15:07:27 +0530

    final version

--- src/main/java/gamingappflow/controller/MainClass.java
+++ src/main/java/gamingappflow/controller/MainClass.java
@@ -16,52 +16,59 @@ public class MainClass { // controller package
 		HibernateLogic logic = new HibernateLogic();
 		boolean flag = true;
 		while (flag) {
-			System.out.println("1.Play the Game\n2.Fetch Object\n3.Delete Object\n4.update Object name\n5.Exit\nEnter the valid option......");
+			System.out.println(
+					"1.Play the Game\n2.Fetch Object\n3.Delete Object\n4.update Object name\n5.Exit\nEnter the valid option......");
 			switch (sc.nextInt()) {
 			case 1: {
 				Player p = new Player();
 				System.out.println("Enter Name");
-				Scanner sc3=new Scanner(System.in);
+				Scanner sc3 = new Scanner(System.in);
 				p.setName(sc3.nextLine());
 				System.out.println("Enter Age");
-				p.setAge(sc.nextInt());
-				System.out.println("Enter mail id");
-				p.setEmail(sc.next());
-				System.out.println("Enter password");
-				p.setPassword(AES.encrypt(sc.next(), "77"));
-				System.out.println("Enter the image path");
-				Scanner sc2 = new Scanner(System.in);
-				FileInputStream stream = new FileInputStream(sc2.nextLine());
-				byte[] arr = new byte[stream.available()];
-				stream.read(arr);
-				p.setImage(arr);
-				System.out.println("Enter Phone number");
-				p.setPhNumber(sc.nextLong());
-				// ---------------------------------------------------------------
-				System.out.println("Enter the Amount to deposite");
-				double depositedAmount = sc.nextDouble();
-				p.setDepositedAmount(depositedAmount);
-				System.out.println("All the best");
-				RandomNumberGenerator generator = new RandomNumberGenerator();
-				int[] randomNumbers = generator.generator();
-				double amountPreMatch = depositedAmount / 3;
-				double totalEarnedAmount = 0;
-				double profit = 0;
-				for (int i = 0; i <= 2; i++) {
-					System.out.println("Enter number");
-					int no1 = sc.nextInt();
-					if (no1 == randomNumbers[i]) {
-						profit = profit + amountPreMatch;
-						System.out.println("Congraluation..................");
-					} else {
-						depositedAmount = depositedAmount - amountPreMatch;
-						System.out.println("oops wrong guess");
+				int age = sc.nextInt();
+				if (age < 18) {
+					System.out.println("ur not allowed to play");
+					break;
+				} else {
+					p.setAge(sc.nextInt());
+					System.out.println("Enter mail id");
+					p.setEmail(sc.next());
+					System.out.println("Enter password");
+					p.setPassword(AES.encrypt(sc.next(), "77"));
+					System.out.println("Enter the image path");
+					Scanner sc2 = new Scanner(System.in);
+					FileInputStream stream = new FileInputStream(sc2.nextLine());
+					byte[] arr = new byte[stream.available()];
+					stream.read(arr);
+					p.setImage(arr);
+					System.out.println("Enter Phone number");
+					p.setPhNumber(sc.nextLong());
+					// ---------------------------------------------------------------
+					System.out.println("Enter the Amount to deposite");
+					double depositedAmount = sc.nextDouble();
+					p.setDepositedAmount(depositedAmount);
+					System.out.println("All the best");
+					RandomNumberGenerator generator = new RandomNumberGenerator();
+					int[] randomNumbers = generator.generator();
+					double amountPreMatch = depositedAmount / 3;
+					double totalEarnedAmount = 0;
+					double profit = 0;
+					for (int i = 0; i <= 2; i++) {
+						System.out.println("Enter number");
+						int no1 = sc.nextInt();
+						if (no1 == randomNumbers[i]) {
+							profit = profit + amountPreMatch;
+							System.out.println("Congraluation..................");
+						} else {
+							depositedAmount = depositedAmount - amountPreMatch;
+							System.out.println("oops wrong guess");
+						}
 					}
+					totalEarnedAmount = profit + depositedAmount;
+					p.setProfit(profit);
+					p.setTotalearnedAmount(totalEarnedAmount);
+					logic.addObject(p);
 				}
-				totalEarnedAmount = profit + depositedAmount;
-				p.setProfit(profit);
-				p.setTotalearnedAmount(totalEarnedAmount);
-				logic.addObject(p);
 			}
 				break;
 			case 2: {
